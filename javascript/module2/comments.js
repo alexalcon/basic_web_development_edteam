1c540bcda5a4d8295ced8441039afdb54df45bca
alexalcon <alexalcon1596@gmail.com>
2025-12-11 12:36:33 -0400

    JavaScript - Module 2: Add initial HTML and JavaScript files with comments

---
+++ javascript/module2/comments.js
@@ -0,0 +1,30 @@
+// single line comment
+
+/* 
+    multi 
+    line 
+    comment
+*/
+
+/**
+ * multi 
+ * line 
+ * comment 
+ * with 
+ * asterisk
+ */
+
+// making the following for multi line comments is a bit tedious (not optimal):
+// multi
+// line
+// comment
+
+// do not abuse comments; write clean code instead
+
+// it is a good practice to comment out code line by line when debugging
+// unless you are commenting out large blocks of code (like functions)
+//----------------------------------------------------------------------
+// var hello = "greetings"; // inline comment
+// console.log(hello);
+// console.log("bye");
+//----------------------------------------------------------------------
